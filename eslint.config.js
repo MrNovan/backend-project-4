3842d8c3b2e41c0cf0a0cf35febb56e8ff5237e4
MrNovan <toni.justdoit@bk.ru>
2025-06-07 17:58:18 +0300

    add file

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,7 @@
-import globals from 'globals';
-
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import pluginJs from '@eslint/js';
-import importPlugin from 'eslint-plugin-import';
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
@@ -15,43 +12,45 @@ const compat = new FlatCompat({
 });
 
 export default [
+  {/* languageOptions: { globals: globals.node } */},
+  /*   ...compat.extends("airbnb-base", "plugin:jest/recommended"),
+  ...compat.env({ node: true }),
+  ...compat.plugins("jest"), */
+  ...compat.config({
+    plugins: ['jest'],
+    extends: ['airbnb-base', 'plugin:jest/recommended'],
+    env: {
+      node: true,
+      es2020: true,
+    },
+    rules: {
+      'import/extensions': 0,
+      'import/no-named-as-default': 0,
+      'import/no-named-as-default-member': 0,
+    },
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+  }),
   {
+    ignores: ['temp.js', '__fixtures__/*', 'eslint.config.js'],
+  },
+  /* {
+    rules: {
+      "import/extensions": 0,
+    }
+  },
+  {
+    plugins: { jest, importPlugin },
+    rules: {
+      ...jest.configs.recommended.rules,
+      ...importPlugin.configs.recommended.rules
+    },
     languageOptions: {
       globals: {
-        ...globals.node,
         ...globals.jest,
-      },
-      parserOptions: {
-        // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
-        // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-    plugins: { import: importPlugin },
-    rules: {
-      ...importPlugin.configs.recommended.rules,
-    },
-  },
-  ...compat.extends('airbnb-base'),
-  {
-    rules: {
-      'no-underscore-dangle': [
-        'error',
-        {
-          allow: ['__filename', '__dirname'],
-        },
-      ],
-      'import/extensions': [
-        'error',
-        {
-          js: 'always',
-        },
-      ],
-      'import/no-named-as-default': 'off',
-      'import/no-named-as-default-member': 'off',
-      'no-console': 'off',
-      'import/no-extraneous-dependencies': 'off',
-    },
-  },
+      }
+    }
+  } */
 ];
