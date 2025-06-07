87e0a2d66f86ed7ce0fb1e904d92d032099eca75
MrNovan <toni.justdoit@bk.ru>
2025-06-07 12:57:30 +0300

    fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -4,30 +4,29 @@ import vitest from '@vitest/eslint-plugin';
 import vitestGlobals from 'eslint-plugin-vitest-globals';
 import stylistic from '@stylistic/eslint-plugin';
 
-const baseConfig = {
-  languageOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-  plugins: {
-    '@stylistic': stylistic,
-    vitest,
-    'vitest-globals': vitestGlobals,
-  },
-  rules: {
-    // Stylistic
-    '@stylistic/indent': ['error', 2],
-    '@stylistic/linebreak-style': ['error', 'unix'],
-    '@stylistic/no-trailing-spaces': 'error',
-    '@stylistic/eol-last': ['error', 'always'],
-    '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }],
-
-    // Best Practices
-    'no-debugger': 'error',
-    'no-underscore-dangle': ['error', { allow: ['__filename', '__dirname'] }],
-  },
-};
-
 export default [
-  baseConfig,
+  {
+    files: ['**/*.{js,ts}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    plugins: {
+      '@stylistic': stylistic,
+      vitest,
+      'vitest-globals': vitestGlobals,
+    },
+    rules: {
+      // Stylistic
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/linebreak-style': ['error', 'unix'],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }],
+
+      // Best Practices
+      'no-debugger': 'error',
+      'no-underscore-dangle': ['error', { allow: ['__filename', '__dirname'] }],
+    },
+  },
 ];
