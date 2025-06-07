acdfcfb42d9e40edf78d1a546e51b7104d3f547a
MrNovan <toni.justdoit@bk.ru>
2025-06-07 12:47:32 +0300

    fix lint

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+// eslint.config.js
+
+import vitest from '@vitest/eslint-plugin';
+import vitestGlobals from 'eslint-plugin-vitest-globals';
+import stylistic from '@stylistic/eslint-plugin';
+
+const baseConfig = {
+  languageOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: {
+    '@stylistic': stylistic,
+    vitest,
+    'vitest-globals': vitestGlobals,
+  },
+  rules: {
+    // Stylistic
+    '@stylistic/indent': ['error', 2],
+    '@stylistic/linebreak-style': ['error', 'unix'],
+    '@stylistic/no-trailing-spaces': 'error',
+    '@stylistic/eol-last': ['error', 'always'],
+    '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }],
+
+    // Best Practices
+    'no-debugger': 'error',
+    'no-underscore-dangle': ['error', { allow: ['__filename', '__dirname'] }],
+  },
+};
+
+export default [
+  baseConfig,
+];
