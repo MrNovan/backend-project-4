a9eb4d3597d2b53274b7a2e203402732537cbab7
MrNovan <toni.justdoit@bk.ru>
2025-06-07 15:54:06 +0300

    delete all

--- client/client.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const get = (url, params = {}) => axios.get(url, { params }).then((response) => response.data);
-
-const httpClient = {
-  get,
-};
-
-export default httpClient;
