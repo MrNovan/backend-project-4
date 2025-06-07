4b9432d37c6f0504c572cab4ab2b39c234553461
MrNovan <toni.justdoit@bk.ru>
2025-06-07 15:16:18 +0300

    plug

--- src/pageLoader.js
+++ src/pageLoader.js
@@ -1,19 +1,26 @@
-import fsp from 'fs/promises';
-import path from 'path';
 import os from 'os';
+import path from 'path';
+import { promises as fsp } from 'fs';
 
-import {
-  getLocalName, downloadHtml, downloadResources, replaceLinks,
-} from './utils.js';
+import getLocalName from './getLocalName.js';
+import downloadHtml from './downloadHtml.js';
+import downloadResources from './downloadResources.js';
+import replaceLinks from './replaceLinks.js';
 
-export default (url, outputDir = process.cwd()) => {
+export default function loadPage(url, outputDir = process.cwd()) {
+  // Проверка типа outputDir
+  if (typeof outputDir !== 'string') {
+    return Promise.reject(new TypeError('outputDir must be a string'));
+  }
+
   const normalizedOutputDir = outputDir.replace(/^~/, os.homedir());
   const filename = getLocalName(url, 'html');
   const filepath = path.resolve(normalizedOutputDir, filename);
 
-  return downloadHtml(url, outputDir)
+  return downloadHtml(url, normalizedOutputDir)
     .then((htmlPath) => downloadResources(url, normalizedOutputDir, htmlPath))
     .then((imgPaths) => replaceLinks(filepath, imgPaths))
     .then((html) => fsp.writeFile(filepath, html))
     .then(() => filepath);
-};
+}
+
