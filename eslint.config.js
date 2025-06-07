3808e13ae153f60751012bf90ce8cb771a4ec9a7
MrNovan <toni.justdoit@bk.ru>
2025-06-07 18:02:04 +0300

    add file

--- eslint.config.js
+++ eslint.config.js
@@ -1,56 +1,32 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import pluginJs from '@eslint/js';
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
+import pluginJest from 'eslint-plugin-jest'
 
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: pluginJs.configs.recommended,
-});
+export default defineConfig([
+  stylistic.configs.recommended,
 
-export default [
-  {/* languageOptions: { globals: globals.node } */},
-  /*   ...compat.extends("airbnb-base", "plugin:jest/recommended"),
-  ...compat.env({ node: true }),
-  ...compat.plugins("jest"), */
-  ...compat.config({
-    plugins: ['jest'],
-    extends: ['airbnb-base', 'plugin:jest/recommended'],
-    env: {
-      node: true,
-      es2020: true,
-    },
-    rules: {
-      'import/extensions': 0,
-      'import/no-named-as-default': 0,
-      'import/no-named-as-default-member': 0,
-    },
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-  }),
   {
-    ignores: ['temp.js', '__fixtures__/*', 'eslint.config.js'],
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.node,
+    },
   },
-  /* {
-    rules: {
-      "import/extensions": 0,
-    }
-  },
+
   {
-    plugins: { jest, importPlugin },
-    rules: {
-      ...jest.configs.recommended.rules,
-      ...importPlugin.configs.recommended.rules
+    files: ['**/*.test.{js,mjs,cjs}'], // только тестовые файлы
+    plugins: {
+      jest: pluginJest,
     },
     languageOptions: {
       globals: {
-        ...globals.jest,
-      }
-    }
-  } */
-];
+        ...globals.node,
+        ...pluginJest.environments.globals.globals,
+      },
+    },
+    rules: pluginJest.configs.recommended.rules,
+  },
+])
