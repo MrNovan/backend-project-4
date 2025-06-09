1df78697094cf445a7a460ef20dcbe9b65a0489e
MrNovan <toni.justdoit@bk.ru>
2025-06-09 13:25:30 +0300

    fix file

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,8 @@
-import globals from 'globals';
-import js from '@eslint/js';
-import importPlugin from 'eslint-plugin-import';
-import * as espree from 'espree';
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
 
-console.log('Running eslint.config.js');
-
-export default [
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        parser: espree,
-      },
-    },
-    plugins: {
-      import: importPlugin,
-    },
-    rules: {
-      ...importPlugin.configs.recommended.rules,
-
-      'no-underscore-dangle': ['error', { allow: ['__filename', '__dirname'] }],
-      'import/extensions': ['error', 'always', { js: 'always' }],
-      'import/no-named-as-default': 'off',
-      'import/no-named-as-default-member': 'off',
-      'no-console': 'off',
-      'import/no-extraneous-dependencies': 'off',
-    },
-  },
-  {
-    ...js.configs.recommended,
-    rules: {
-      ...js.configs.recommended.rules,
-      // можно переопределить правила здесь при необходимости
-    },
-  },
-];
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.browser } },
+])
